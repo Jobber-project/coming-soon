4db4bf81ce649c0b6a507b3414e1340111e33b4c
Jesper Johansson <johansson.jesper@outlook.com>
2021-02-16 20:54:07 +0100

    Add gatsby-plugin-google-analytics dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    plugins: [
-
-    ]
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'G-BD1NXEB34Z',
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+        pageTransitionDelay: 0,
+        defer: true,
+        cookieDomain: 'jobelo.se',
+      },
+    },
+  ],
 }

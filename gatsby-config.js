e0b66ab9979254a03a1fdd0e86e5f268f75a0ccb
Jesper Johansson <johansson.jesper@outlook.com>
2021-02-16 20:58:06 +0100

    Add gatsby-plugin-manifest dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,17 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Jobelo',
+        short_name: 'Jobelo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+      },
+    },
+    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'G-BD1NXEB34Z',

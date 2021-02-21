e50437152308626c95e3bcfdc72668278c6129fb
Willie <Willie.bjornbom@ancon.io>
2021-02-21 09:53:31 +0100

    Added gradient and reload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Gradient = styled.div`
+  background: linear-gradient(90deg, #775dfa 23.06%, #5a9ef5 98.75%);
+  background-attachment: fixed;
+  min-height: 100%;
+
+  width: 100%;
+  height: auto;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+`
 
 export default function IndexPage() {
-  return <div>Coming soon</div>
+  return (
+    <Gradient>
+      <h1>Coming soon</h1>
+    </Gradient>
+  )
 }

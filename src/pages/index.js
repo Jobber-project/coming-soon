5c41587bcb37f50bbd051792e256452a40b27706
Willie <Willie.bjornbom@ancon.io>
2021-02-21 13:30:39 +0100

    Added confetti

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 
-import Confetti from '../feature/Confetti/Confetti'
+import Welcome from '../feature/Confetti/Confetti'
 
 const Background = styled.div`
   background: linear-gradient(90deg, #775dfa 23.06%, #5a9ef5 98.75%);
@@ -29,7 +29,7 @@ export default function IndexPage() {
       </Helmet>
       <Background>
         <h1>Coming soon</h1>
-        <Confetti />
+        <Welcome />
       </Background>
     </>
   )

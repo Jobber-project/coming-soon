9d62f1f75eaaf6072639a569952e26676b87a6ce
Jesper Johansson <johansson.jesper@outlook.com>
2021-02-25 21:42:44 +0100

    Remove waves animation, add static wave image

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import WaveImg from '../images/wave.svg'
+
 import DotSquare from './decoration/DotSquare'
 import Waves from './decoration/Waves'
-import Wave from './Wave'
 
 const Container = styled.div`
   z-index: -1;
@@ -14,6 +15,7 @@ const Container = styled.div`
   left: 0;
   background-image: linear-gradient(90deg, #775dfa 23.06%, #5a9ef5 98.75%);
   pointer-events: none;
+  overflow: hidden;
 `
 
 const Inner = styled.div`
@@ -23,11 +25,23 @@ const Inner = styled.div`
   height: 100%;
 `
 
+const Img = styled.img`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
 export default function Background() {
   return (
     <Container aria-hidden="true">
       <Inner>
-        <Wave />
+        <Img src={WaveImg} alt="gradient" />
         <DotSquare />
         <Waves />
       </Inner>

82ad106981176c7d52a2b0f3fc7fba2be2ac3ee7
Willie <Willie.bjornbom@ancon.io>
2021-03-14 13:24:44 +0100

    Added media queries

--- src/components/Punchline.js
+++ src/components/Punchline.js
@@ -2,6 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled, { css, keyframes } from 'styled-components'
 
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+}
+
+export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
+}
+
 const animation = keyframes`
   0% {
     opacity: 0;
@@ -30,38 +51,48 @@ const PunchlineWrapper = styled.div`
   align-items: center;
   justify-content: center;
 
-  max-width: 600px;
+  max-width: 80%;
 `
 
 const Title = styled.h1`
   font-weight: bold;
-  font-size: 6.4rem;
+  font-size: 4rem;
   letter-spacing: 0.12em;
   margin: 0;
 
   color: #ffffff;
+  @media ${device.laptop} {
+    font-size: 6.4rem;
+  }
 `
 
 const Header = styled.h3`
   font-weight: bold;
-  font-size: 3.6rem;
+  font-size: 2.5rem;
   text-align: center;
   letter-spacing: 0.12em;
   margin: 0;
 
   color: #ffffff;
   margin-top: 20px;
+
+  @media ${device.laptop} {
+    font-size: 3.6rem;
+  }
 `
 
 const Paragraph = styled.p`
   font-weight: 300;
-  font-size: 1.6rem;
+  font-size: 1.2rem;
   text-align: center;
   line-height: 200%;
   margin: 0;
 
   color: white;
-  margin-top: 30px;
+  margin-top: 10px;
+  @media ${device.laptop} {
+    font-size: 1.6rem;
+  }
 `
 
 export default function Punchline({ title, header, paragraph, children }) {

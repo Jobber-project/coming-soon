68da3bbadd02a107e15a6e68e678d4ad1f796124
Willie <Willie.bjornbom@ancon.io>
2021-05-02 12:14:59 +0200

    Added fetch

--- src/features/graphql/client.js
+++ src/features/graphql/client.js
@@ -1,8 +1,14 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import fetch from 'isomorphic-fetch'
+
+export const link = createHttpLink({
+  fetch,
+  uri: 'https://dev-jobber-api-l.azurewebsites.net/graphql',
+})
 
 const client = new ApolloClient({
-  uri: 'https://dev-jobber-api-l.azurewebsites.net/graphql',
   cache: new InMemoryCache(),
+  link,
 })
 
 export default client

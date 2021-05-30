21c16535e1bc830edcd126bcc2a694508892a27a
Willie <Willie.bjornbom@ancon.io>
2021-05-30 16:58:09 +0200

    Updates

--- component---src-pages-index-js-585fd036c485ec983d04.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var o=e("q1tI"),u=e.n(o);function c(){return u.a.createElement("div",null,"Coming soon")}}}]);
-//# sourceMappingURL=component---src-pages-index-js-585fd036c485ec983d04.js.map
